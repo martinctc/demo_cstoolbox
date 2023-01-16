2bff1a98de471f3c8bb92503b37bde8548c9739d
Martin Chan <martinchan53@gmail.com>
2023-01-16 17:44:22 +0000

    feat: add `ClassifersTest.cs`

---
+++ ClassifersTest.cs
@@ -0,0 +1,16 @@
+﻿using cstoolbox;
+
+public class ClassifiersTest
+{
+    public void TestClassifyAge(int age)
+    {
+        cstoolbox.Classifiers c_alg = new Classifiers();        
+        string result = c_alg.ClassifyAge(age);        
+        Console.WriteLine("Testing 'ClassifyAge' method: ");
+        Console.Write("Input age:");
+        Console.WriteLine(age);
+        Console.Write("Output:");
+        Console.WriteLine(result);
+        Console.WriteLine("...");
+    }
+}
