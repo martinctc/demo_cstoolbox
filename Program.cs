9359078c4e625dd0cf5403a674f752fd92c0d4b8
Martin Chan <martinchan53@gmail.com>
2023-01-16 17:44:37 +0000

    format: print header

--- Program.cs
+++ Program.cs
@@ -3,14 +3,25 @@ using System;
 using cstoolbox;
 public class Test
 {   
-    static void Main()
+    static void Main()        
     {
+        // Print header
+        Console.WriteLine("Running tests for `cstoolbox`: ");
+        Console.WriteLine("==============================================================");
+        Console.WriteLine("...");
+
+        // Test Algebra class
         AlgebraTest test = new AlgebraTest();
         test.TestAddition(100, 50);
         test.TestDivision(100, 3);
         
+        // Test String Manipulation class
         StringManipulationTest test2 = new StringManipulationTest();
         test2.TestReplaceSpaceWithUnderscore("Make Me A Camel Case");
+
+        // Test Classifier class
+        ClassifiersTest test3 = new ClassifiersTest();
+        test3.TestClassifyAge(20);
 
     }
 }
